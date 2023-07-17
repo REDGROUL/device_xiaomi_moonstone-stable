89ea17e56756f61106ff7aec80e67927e01933fc
REDGROUL <38361748+REDGROUL@users.noreply.github.com>
2023-07-17 11:55:02 +0300

    derp-ify

--- derp_moonstone.mk
+++ derp_moonstone.mk
@@ -9,15 +9,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from moonstone device
 $(call inherit-product, device/xiaomi/moonstone/device.mk)
 
-PRODUCT_NAME := lineage_moonstone
+PRODUCT_NAME := derp_moonstone
 PRODUCT_DEVICE := moonstone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X5 5G
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
